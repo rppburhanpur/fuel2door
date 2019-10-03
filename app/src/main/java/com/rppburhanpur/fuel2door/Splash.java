b56b5efe7e0c68cb629669db588ebf702d9c6d72
Mohit Varma <mv501049@gmail.com>
2019-10-03 18:15:54 +0530

    updated

--- app/src/main/java/com/rppburhanpur/fuel2door/Splash.java
+++ app/src/main/java/com/rppburhanpur/fuel2door/Splash.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Splash extends AppCompatActivity {
 
@@ -15,6 +17,8 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         mHandler = new Handler();
         mHandler.postDelayed(new SplashRunner(this),3000);
 
--- app/src/main/java/com/rppburhanpur/fuel2door/User_Registration.java
+++ app/src/main/java/com/rppburhanpur/fuel2door/User_Registration.java
@@ -3,6 +3,9 @@ package com.rppburhanpur.fuel2door;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class User_Registration extends AppCompatActivity {
 
@@ -10,7 +13,6 @@ public class User_Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user__registration);
-
 
     }
 }
